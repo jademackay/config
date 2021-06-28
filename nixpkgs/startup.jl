fb7dbd6c0fd6d27cc7120c1df61d2d00a0ec6d7e
Jade Mackay <jademackay@gmail.com>
2021-06-28 14:42:02 +1200

    julia startup file

---
+++ nixpkgs/startup.jl
@@ -0,0 +1,11 @@
+using Pkg
+
+if isfile("Project.toml") && isfile("Manifest.toml")
+    Pkg.activate(".")
+end
+
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise" exception=(e, catch_backtrace())
+end
