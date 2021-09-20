2549441729821ead813f1284719a42cd8e576d25
Jade Mackay <jademackay@gmail.com>
2021-09-21 08:24:40 +1200

    Add a lite julia

---
+++ nixpkgs/julia-1.6.2_tmp.nix
@@ -0,0 +1,61 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+	juliatree=pkgs.stdenv.mkDerivation {
+		name = "juliatree";
+    version = "1.6.2";
+    src = pkgs.fetchzip {
+      url="https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.2-linux-x86_64.tar.gz";
+      sha256="sha256:0dhwllf8zjggfzd0h63pxpcp1dwiq4i8i3fiasz61sfvmkyshh83";
+		};
+		buildPhase = ''
+		           	'';
+		installPhase = ''
+		          mkdir -p $out
+					    cp -r $src $out/julia
+					    '';         
+		fixupPhase = ''
+					    '' ;
+  };
+  ourpython=(pkgs.python38.withPackages (ps: [ps.matplotlib]));
+  # ATTN: python version must conform with the export PYTHONPATH path in `profile` below
+in
+pkgs.buildFHSUserEnv {
+  name = "julia";
+  targetPkgs = pkgs: (
+		with pkgs;
+		[
+			juliatree
+			curl
+			qt4
+			coreutils
+			ourpython
+      gcc
+      binutils-unwrapped
+      cmdstan # change this to local cmdstan bc stan execution writes to
+      # $CMDSTAN_HOME/stan/src/stan/model/
+      # Fairness.jl:
+      ipopt
+      # TikzPictures.jl:
+      texlive.combined.scheme-full # full bc already installed
+      # Conda.jl: (still failing)
+      zlib
+      conda
+      adoptopenjdk-jre-bin
+		]
+	);
+  profile = ''
+    export PATH=${juliatree}/julia/bin:$PATH
+    export PYTHONPATH=${ourpython}/lib/python3.8/site-packages  
+    export PYTHON=${ourpython}/bin/python
+    export LIBRARY_PATH=${pkgs.gcc}:$LIBRARY_PATH 
+    export LIBRARY_PATH=${pkgs.ipopt}:$LIBRARY_PATH
+    export LIBRARY_PATH=${pkgs.zlib}:$LIBRARY_PATH
+    export LD_LIBRARY_PATH=${pkgs.zlib}:$LD_LIBRARY_PATH
+    export PATH=${pkgs.binutils-unwrapped}/bin:$PATH
+    export JULIA_CMDSTAN_HOME=${pkgs.cmdstan}/opt/cmdstan
+  '';     
+  runScript="julia";
+  # TODO:
+  # * Add Revise and Documenter and Plots and and PyPlot and PyCall here
+}
+  
